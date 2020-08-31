1e79cd9889dc4d759d75a273cec74c2b2afc07bc
CSBlues <66141547+CSBlues@users.noreply.github.com>
2020-08-31 09:18:17 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int birthYear = 0;
+    int currentYear = 0;
+    int dogBirthYear = 0;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your birth year: ");
+    // TODO: Set birth year equal to the input value.
+
+    System.out.println("Enter your dog's birth year: ");
+    // TODO: Set dogBirthYear equal to the input.
+    
+    System.out.println("Enter the current year: ");
+    // TODO: Set the currentYear equal to the input.
+
+
+    /* TODO: 
+    Implement the algorithms to calculate your 
+    age and your dog's age.
+    */
+    
+    /* TODO:
+    Print your calculations for each age. 
+    Remember your out put should be in the form 
+    Human: 73 Dog: 63
+    */
+    
+  }
+}
